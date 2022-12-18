0b2c15cbdd4d9554c2235c796feaee0c8a088b76
ahackerotto <119680017+ahackerotto@users.noreply.github.com>
2022-12-18 18:50:15 +0100

    update API key

--- src/index.js
+++ src/index.js
@@ -77,7 +77,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "6643c7326a4c2a38838264a28531d97e";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
